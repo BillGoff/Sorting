7be627b63b5d210801c8a28f3db48f5eb4df602c
bgoff <bgoff@localhost>
2024-11-26 10:24:20 -0500

    updated.

--- src/main/java/sorting/Sort.java
+++ src/main/java/sorting/Sort.java
@@ -2,15 +2,11 @@ package sorting;
 
 import java.time.Duration;
 import java.time.Instant;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.HashMap;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
 import java.util.Random;
-import java.util.stream.Stream;
 
 
 /**
